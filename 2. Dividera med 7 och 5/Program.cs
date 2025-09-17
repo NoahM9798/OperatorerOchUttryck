c8dc84de1ce6593107e4d5bcaa9415e2d0ddc60b
Noah Mattsson <mail@noahmattsson.se>
2025-09-17 09:33:40 +0200

    Lagt till övning 2

--- 2. Dividera med 7 och 5/Program.cs
+++ 2. Dividera med 7 och 5/Program.cs
@@ -1,3 +1,8 @@
 ﻿/*
- 
+ Skriv ett booleskt uttryck som kollar om ett givet heltal är jämt delbart med 5 och 7 samtidigt
  */
+
+Console.Write("Ange ett heltal: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Talet {0} är delbart med 5 och 7 samtidigt: {1}", number, number % 35 == 0);
