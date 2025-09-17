e5b4471b34168400c44b2c3889255fabfb9653aa
Noah Mattsson <mail@noahmattsson.se>
2025-09-17 09:53:57 +0200

    Lagt till uppgift 3

---
+++ 3. Beräkna rektangels area/Program.cs
@@ -0,0 +1,10 @@
+﻿/*
+ Skriv ett uttryck som beräknar area av en rektangel utifrån inmatade höjd och bredd
+ */
+
+Console.Write("Ange bredd: ");
+double bredd = double.Parse(Console.ReadLine());
+Console.Write("Ange höjd: ");
+double höjd = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Rektangelns area: {bredd * höjd}");
