02e3a5f09cbce34fc7dd01c26f62b1856496c9c3
Noah Mattsson <mail@noahmattsson.se>
2025-09-17 09:16:04 +0200

    Lagt till kod för övning 1

--- 1. Udda eller Jämt/Program.cs
+++ 1. Udda eller Jämt/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿/*
+ Skriv ett program som kollar om ett givet heltal är jämt eller udda
+ */
+Console.Write("Ange ett heltal: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Talet är {0} är {1}", number, number % 2 == 0 ? "jämt" : "udda");
